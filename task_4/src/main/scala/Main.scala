a2108ab5f6292fbf7cf96266a037048a81690116
Igor Titov <titov@phystech.edu>
2018-05-21 01:58:31 +0300

    add solution for task_4

--- task_4/src/main/scala/Main.scala
+++ task_4/src/main/scala/Main.scala
@@ -1,58 +1,114 @@
 import java.lang
-import java.util.StringTokenizer
+import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.{Date, StringTokenizer}
 
+import Main.{OutputValueMapperClass, Utils}
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.io.{BytesWritable, Text, UTF8}
 import org.apache.hadoop.mapreduce.{Job, Mapper, Reducer}
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 
+import scala.collection.JavaConversions._
+import scala.util.{Failure, Success, Try}
 
-object WordCount extends App {
 
-  object TokenizerMapper {
-    private val one = new IntWritable(1)
-  }
+object Main extends App {
+  type InputKeyMapperClass = AnyRef
+  type InputValueMapperClass = Text
+  type OutputKeyMapperClass = Text
+  type OutputValueMapperClass = Text
+  type OutputKeyReducerClass = Text
+  type OutputValueReducerClass = Text
+  type MapperType = Mapper[InputKeyMapperClass, InputValueMapperClass, OutputKeyMapperClass, OutputValueMapperClass]
+  type ReducerType = Reducer[OutputKeyMapperClass, OutputValueMapperClass, OutputKeyReducerClass, OutputValueReducerClass]
 
-  class TokenizerMapper extends Mapper[AnyRef, Text, Text, IntWritable] {
-    private val word = new Text
-
-    override def map(key: AnyRef, value: Text, context: Mapper[AnyRef, Text, Text, IntWritable]#Context): Unit = {
-      val itr = new StringTokenizer(value.toString)
-      while ( {
-        itr.hasMoreTokens
-      }) {
-        word.set(itr.nextToken)
-        context.write(word, TokenizerMapper.one)
+  class TokenizerMapper extends MapperType {
+    override def map(key: AnyRef, value: Text, context: MapperType#Context): Unit = {
+      Try {
+        val itr = new StringTokenizer(value.toString, "\t")
+        val id = itr.nextToken
+        val rawData = itr.nextToken
+        val domain = Utils.getDomain(itr.nextToken)
+        context.write(new OutputKeyMapperClass(id), new OutputValueMapperClass(Utils.stringPairAsText(domain, rawData.toString)))
+      }.getOrElse {
+        context.write(null, null)
       }
+
     }
   }
 
-  class IntSumReducer extends Reducer[Text, IntWritable, Text, IntWritable] {
-    private val result = new IntWritable
+  class MyReducer extends ReducerType {
+    override def reduce(key: OutputKeyMapperClass, values: lang.Iterable[OutputValueMapperClass], context: ReducerType#Context): Unit = {
+      Try {
+        val seq = values.iterator().toList
 
-    override def reduce(key: Text, values: lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
-      var sum = 0
-      import scala.collection.JavaConversions._
-      for (value <- values) {
-        sum += value.get
-      }
-      result.set(sum)
-      context.write(key, result)
+        def trimTodd(str: String): String = {
+          str.take(10)
+        }
+
+        val count = seq.map(Utils.textAsStringPair)
+          .map(v => v._1 + TestUtils.separator + trimTodd(v._2))
+          .distinct
+          .map(TestUtils.textAsStringPair)
+          .groupBy(_._2)
+          .mapValues(_.map(_._1).distinct)
+          .values.maxBy(_.length)
+
+        context.write(new OutputKeyReducerClass(key.toString + ":"), new OutputValueReducerClass( "[" + count.mkString(",") + "]"))
+      }.getOrElse(())
     }
-  }
+
 
     val conf = new Configuration
     val job = new Job(conf, "word count")
-    job.setJarByClass(WordCount.getClass)
-    job.setMapperClass(classOf[WordCount.TokenizerMapper])
-    job.setCombinerClass(classOf[WordCount.IntSumReducer])
-    job.setReducerClass(classOf[WordCount.IntSumReducer])
-    job.setOutputKeyClass(classOf[Text])
-    job.setOutputValueClass(classOf[IntWritable])
+    job.setJarByClass(Main.getClass)
+    job.setMapperClass(classOf[Main.TokenizerMapper])
+    job.setCombinerClass(classOf[Main.MyReducer])
+    job.setReducerClass(classOf[Main.MyReducer])
+    job.setOutputKeyClass(classOf[OutputKeyReducerClass])
+    job.setOutputValueClass(classOf[OutputValueReducerClass])
     FileInputFormat.addInputPath(job, new Path(args(0)))
     FileOutputFormat.setOutputPath(job, new Path(args(1)))
     System.exit(if (job.waitForCompletion(true)) 0
     else 1)
+  }
+
+  object Utils {
+    def getDomain(url: String): String = {
+      val uri = Try(new URI(url)).getOrElse(urlFromStringWithIllegalCharacters(url))
+      val host = uri.getHost
+      if (host.startsWith("www.")) host.drop(4) else host
+    }
+
+    def getJavaDate(time: String): Date = {
+      val formatter = new SimpleDateFormat("yyyy MM dd HH:mm:ss.SSS")
+      formatter.parse(time)
+    }
+
+    def stringPairAsText(v1: String, v2: String): Text = {
+      val str = v1 + separator + v2
+      new Text(str.getBytes)
+    }
+
+    def textAsStringPair(text: OutputValueMapperClass): (String, String) = {
+      val arr = text.toString.split(Utils.separator)
+      (arr(0), arr(1))
+    }
+
+    private def urlFromStringWithIllegalCharacters(str: String): URI = {
+      val ponetnialUri = str.dropRight(1)
+      Try(new URI(ponetnialUri)) match {
+        case Success(uri) => new URI(ponetnialUri)
+        case Failure(_: java.net.URISyntaxException) => urlFromStringWithIllegalCharacters(ponetnialUri)
+        case _ => new URI("")
+      }
+    }
+
+    val separator = "\t"
+
+  }
+
 }
